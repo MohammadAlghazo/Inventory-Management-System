7124d2dee1b300d3d00a7db9bbc04d93b72aad06
Mohammad Alghazo <mohammadalghazo2004m@gmail.com>
2026-01-27 19:41:47 +0300

    Implement product listing page, integrate API, and configure CORS

--- FrontEnd/InventoryManagment/src/app/pages/products/products.component.ts
+++ FrontEnd/InventoryManagment/src/app/pages/products/products.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
+  products: Product[] = []; 
+
+  constructor(private productService: ProductService) {} 
+
+  ngOnInit(): void {
+    this.loadProducts(); 
+  }
+
+  loadProducts() {
+    this.productService.getProducts().subscribe({
+      next: (data) => {
+        this.products = data; 
+        console.log('Products Loaded:', this.products); 
+      },
+      error: (err) => {
+        console.error('Error fetching products:', err); 
+      }
+    });
+  }
 }
