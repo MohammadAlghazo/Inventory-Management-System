04fc3364c54507f55152887f82cf011f0e1d2cca
Mohammad Alghazo <mohammadalghazo2004m@gmail.com>
2026-02-01 13:19:16 +0300

    Add Category property

--- Inventory Management/Models/Product.cs
+++ Inventory Management/Models/Product.cs
@@ -7,6 +7,7 @@
         public decimal? Price { get; set; }
         public int? Quantity { get; set; } 
         public int? MinQuantity { get; set; }
+        public string Category { get; set; } = string.Empty;
         public ICollection<InventoryLog> InventoryLogs { get; set; } = new List<InventoryLog>();
     }
 }
