4499f3f690ea761216dc7d1c9182d27da4ffc264
Mohammad Alghazo <mohammadalghazo2004m@gmail.com>
2026-02-01 16:51:22 +0300

    Add AuthGuard, Logout functionality, and HTTP Interceptor

--- FrontEnd/InventoryManagment/src/app/app.component.ts
+++ FrontEnd/InventoryManagment/src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ProductsComponent } from './pages/products/products.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, ProductsComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'InventoryManagement';
+  
+  router = inject(Router);
+
+  onLogout() {
+    localStorage.removeItem('token');
+    
+    this.router.navigate(['/login']);
+  }
 }
