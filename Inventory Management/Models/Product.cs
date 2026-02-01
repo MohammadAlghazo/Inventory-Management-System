c206de0a510a0088e01331d6c45c532f4d639b97
Mohammad Alghazo <mohammadalghazo2004m@gmail.com>
2026-02-01 13:40:26 +0300

    Add Description property

--- Inventory Management/Models/Product.cs
+++ Inventory Management/Models/Product.cs
@@ -8,6 +8,7 @@
         public int? Quantity { get; set; } 
         public int? MinQuantity { get; set; }
         public string Category { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public ICollection<InventoryLog> InventoryLogs { get; set; } = new List<InventoryLog>();
     }
 }
