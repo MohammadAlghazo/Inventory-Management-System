2bb4ebefbc0a2f4a7b96bc339894d08334215882
Mohammad Alghazo <mohammadalghazo2004m@gmail.com>
2026-02-01 16:29:15 +0300

    Implement Login Page and Integrate with Auth API

--- FrontEnd/InventoryManagment/src/app/pages/login/login.component.ts
+++ FrontEnd/InventoryManagment/src/app/pages/login/login.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  authService = inject(AuthService);
+  router = inject(Router);
+
+    loginObj: any = {
+        username: '',
+        password: ''
+      };
+
+onLogin() {
+    this.authService.login(this.loginObj).subscribe({
+      next: (res: any) => {
+        console.log("Response:", res);
+        
+        localStorage.setItem('token', res); 
+        
+        alert("Login Success! ✅");
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        console.error(err);
+        alert("Login Failed! ❌");
+      }
+    });
+  }
 }
